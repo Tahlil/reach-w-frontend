94ed410a4c72b4879dbcea8064a741d511c50a97
Tahlil <bsse0803@iit.du.ac.bd>
2022-08-10 12:06:10 +0600

    Done asking question

--- index.mjs
+++ index.mjs
@@ -94,16 +94,11 @@ const OUTCOME = ['Bob wins', 'Draw', 'Alic wins'];
 interact.seeOutcome = async (outcome) => {
   console.log(`The outcome is: ${OUTCOME[outcome]}`);
 }
-console.log('Starting backends...');
-await Promise.all([
-  backend.Alice(ctcAlice, {
-    ...stdlib.hasRandom,
-    // implement Alice's interact object here
-  }),
-  backend.Bob(ctcBob, {
-    ...stdlib.hasRandom,
-    // implement Bob's interact object here
-  }),
-]);
 
-console.log('Goodbye, Alice and Bob!');
+const part = isAlice ? ctc.p.Alice : ctc.p.Bob;
+await part(interact);
+
+const after = await getBalance();
+console.log(`Your balance is ${after}`);
+
+ask.done();
